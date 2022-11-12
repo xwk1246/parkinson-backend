f9a3f780b8b2c31035544f5b25a3bf70a8f35869
xwk1246 <xwk1246@csie.io>
2022-11-12 12:58:17 +0800

    modify user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,12 +17,19 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
             $table->string('email')->unique();
+            $table->enum('gender', ['male', 'female', 'unknown']);
+            $table->date('birthday');
+            $table->string('personal_id')->unique();
+            $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('doctor_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
