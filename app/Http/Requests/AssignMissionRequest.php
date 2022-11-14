01b1f6cbd2a8595e9c38dff7eec1670f883b4549
lo0k0502 <thomas85514@gmail.com>
2022-11-14 15:30:42 +0800

    finish POST /assign-mission

---
+++ app/Http/Requests/AssignMissionRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class AssignMissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = Auth::user();
+        return !!$user && $user->can('assign-mission');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => ['required', function($attribute, $value, $fail) {
+                $user = User::find($value);
+                if (!$user || !$user->hasRole('patient')) {
+                    return $fail('The user id must be a patient\'s id');
+                }
+                if ($user->doctor_id != Auth::id()) {
+                    return $fail('This patient id doesn\'t belongs to this doctor');
+                }
+            }],
+            'due_date' => ['required', 'date', 'after:today'],
+            'categories' => ['required', 'array'],
+            'categories.*' => ['required', 'integer', Rule::in([1, 2, 3, 4]), 'distinct'],
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\MissionController;
 use App\Http\Controllers\RecordController;
 use App\Http\Controllers\UserController;
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('users', UserController::class);
 Route::apiResource('records', RecordController::class);
 Route::apiResource('missions', MissionController::class);
+
+Route::post('assign-mission', [DoctorController::class, 'assign']);
 
 // Route::get('/', function () {
     // return view('welcome');
