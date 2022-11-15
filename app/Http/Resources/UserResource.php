d7eebac93d59c224c39fa3ec88b4c273e176d787
bartchao <59232607+bartchao@users.noreply.github.com>
2022-11-15 15:36:08 +0800

    Remove wrap in UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -12,7 +12,6 @@ class UserResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    public static $wrap = 'user';
 
     public function toArray($request)
     {
