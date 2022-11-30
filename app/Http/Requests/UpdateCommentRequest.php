9424bc677555ee3e085e31761298270621fd2b43
bartchao <59232607+bartchao@users.noreply.github.com>
2022-11-30 15:23:34 +0800

    UpdateCommentRequest:check belongs to doctor

--- app/Http/Requests/UpdateCommentRequest.php
+++ app/Http/Requests/UpdateCommentRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Record;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -27,7 +29,15 @@ class UpdateCommentRequest extends FormRequest
     public function rules()
     {
         return [
-            'record_id' => ['required', Rule::exists('records', 'id')],
+            'record_id' => ['required', Rule::exists('records', 'id'),function ($attribute, $value, $fail) {
+                $user = Record::find($value)->user;
+                if (!$user || !$user->isPatient()) {
+                    return $fail('The user id must be a patient\'s id');
+                }
+                if ($user->doctor_id != Auth::id()) {
+                    return $fail('This patient id doesn\'t belongs to this doctor');
+                }
+            }],
             'status' => ['required',Rule::in(['已檢閱', '待檢閱'])],
             'doctor_comment' => ['required',"max:255", 'string']
         ];
