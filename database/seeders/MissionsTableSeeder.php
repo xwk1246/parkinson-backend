6670b5f3bab9435f26b4074a7607bc3c9a3585de
bartchao <59232607+bartchao@users.noreply.github.com>
2022-11-30 12:17:41 +0800

    Generate more fake data

--- database/seeders/MissionsTableSeeder.php
+++ database/seeders/MissionsTableSeeder.php
@@ -24,24 +24,28 @@ class MissionsTableSeeder extends Seeder
                 'id' => 1,
                 'user_id' => 2,
                 'due_date' => '2022-11-14',
-                'created_at' => NULL,
-                'updated_at' => NULL,
+                'created_at' => '2022-11-13 08:00:00',
             ),
             1 =>
             array (
                 'id' => 2,
                 'user_id' => 2,
                 'due_date' => '2022-11-14',
-                'created_at' => NULL,
-                'updated_at' => NULL,
+                'created_at' => '2022-11-13 08:00:00',
             ),
             2 =>
             array (
                 'id' => 3,
                 'user_id' => 3,
                 'due_date' => '2022-11-14',
-                'created_at' => NULL,
-                'updated_at' => NULL,
+                'created_at' => '2022-11-13 08:00:00',
+            ),
+            3 =>
+            array (
+                'id' => 4,
+                'user_id' => 3,
+                'due_date' => '2022-11-30',
+                'created_at' => '2022-11-13 08:00:00',
             )
         ));
         
