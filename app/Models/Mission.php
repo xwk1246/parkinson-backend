f5067750dedcfab6f664752171074f707a305ea9
bartchao <59232607+bartchao@users.noreply.github.com>
2022-12-07 15:13:31 +0800

    Add mission count support

--- app/Models/Mission.php
+++ app/Models/Mission.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Mission extends Model
 {
     use HasFactory;
+    protected $appends = ['all_records','uploaded_records'];
 
     protected $fillable = [
         'user_id',
@@ -18,4 +20,23 @@ class Mission extends Model
     {
         return $this->hasMany(Record::class);
     }
+    /**
+     * Determine if the user is an administrator.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function getUploadedRecordsAttribute()
+    {
+        return $this->attributes['uploaded_records'] = $this->records()->whereNot('status','未上傳')->count();;
+    }
+    /**
+     * Determine if the user is an administrator.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function getAllRecordsAttribute()
+    {
+        return $this->attributes['all_records'] = $this->records()->count();
+    }
+    
 }
