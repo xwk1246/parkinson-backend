69a5dfdd36f53db3a7fe7b5fb382a1b0480851b5
bartchao <59232607+bartchao@users.noreply.github.com>
2022-12-13 15:17:04 +0800

    Add update-password route and request

---
+++ app/Http/Requests/UpdatePasswordRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Hash;
+
+
+class UpdatePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'password' => [
+                'required', Password::min(8)->letters(), function ($attribute, $value, $fail) {
+                    if ($value === $this->user()->personal_id) {
+                        $fail('密碼不可與帳號相同');
+                    }
+                },function ($attribute, $value, $fail) {
+                    if (Hash::check($value, $this->user()->password)) {
+                        $fail('密碼不可與上次設定相同');
+                    }
+                },
+            ]
+        ];
+    }
+}
